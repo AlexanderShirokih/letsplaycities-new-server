f0244f933fefedf98625bf4d5a1c1c6b05b86983
Alexander Shirokih <alexandershirokih98@gmail.com>
2020-01-08 01:11:41 +0300

    Increase max message size to 300kB

--- src/main/java/ru/quandastudio/lpsserver/config/WebSocketConfiguration.java
+++ src/main/java/ru/quandastudio/lpsserver/config/WebSocketConfiguration.java
@@ -1,10 +1,12 @@
 package ru.quandastudio.lpsserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import ru.quandastudio.lpsserver.websocket.SocketHandler;
 
@@ -14,6 +16,13 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 
 	@Autowired
 	private SocketHandler socketHandler;
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(307200);// 300kB
+		return container;
+	}
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
