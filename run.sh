1a4b137b02c41da01c5d8cbafc5756b08c2a854a
alexander <alexandrshirokih@mail.ru>
2022-01-08 22:42:01 +0300

    update deploy-steps

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-nohup java -jar "lpsserver-1.6.0.jar" --db.user=lps --db.pass=passIef3qfj9 >/dev/null 2>/dev/null &
+nohup java -jar "lpsserver-1.6.0.jar" --db.user=lps --db.pass=KPc9wh4e082f >/dev/null 2>/dev/null &
 
